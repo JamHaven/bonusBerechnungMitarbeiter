5eb99adb36c7a0d8e4de4598cb70334e8e27c0b5
Mario Rubak <mario.rubak@stud.fh-campuswien.ac.at>
2020-02-22 15:58:56 +0100

    Added interactive cli

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+
 public class main {
 
     public static void main(String[] args) throws Exception {
 
         Mitarbeiter mitarbeiter1 = new Mitarbeiter();
-        System.out.println(mitarbeiter1.calculateBonus(120,"sekretARIat"));
+        int firmenzugehoerigkeit;
+        String position;
+        //System.out.println(mitarbeiter1.calculateBonus(120,"sekretARIat"));
 
+
+        // create a scanner so we can read the command-line input
+        Scanner scanner = new Scanner(System.in);
+
+        //  prompt for the user's name
+        System.out.print("Geben Sie Ihre Firmenzugehörigkeit an (0-75): ");
+
+        // get their input as a String
+        firmenzugehoerigkeit = scanner.nextInt();
+
+        // prompt for their age
+        System.out.print("Geben Sie Ihre Position an: ");
+
+        // get the age as an int
+        position = scanner.next();
+
+        System.out.println(mitarbeiter1.calculateBonus(firmenzugehoerigkeit, position.toLowerCase()));
+        System.out.println(String.format("Ihre Firmenzugehoerigkeit: %d, in der Position %s", firmenzugehoerigkeit, position.toLowerCase()));
     }
 }

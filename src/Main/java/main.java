f3a59554f736dfb2c0151f07479f359379f3e3be
cristianciuciu <cri_ciu2@yahoo.com>
2020-02-22 14:49:29 +0100

    main update

---
+++ src/Main/java/main.java
@@ -0,0 +1,15 @@
+package java;
+
+
+public class main {
+
+    public static void main(String[] args){
+
+        Mitarbeiter mitarbeiter1 = new Mitarbeiter(5,Abteilung.SEKRETARIAT);
+
+    }
+
+
+
+
+}
--- src/main/java/main.java
+++
@@ -1,9 +0,0 @@
-package java;
-
-public class main {
-
-    Mitarbeiter mitarbeiter1 = new Mitarbeiter(5,Abteilung.SEKRETARIAT);
-
-
-
-}
